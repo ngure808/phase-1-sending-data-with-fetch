d5fb1ad02cc22c22d9135877bbfa2c10b5cff204
ngure808 <mwangingure808@gmail.com>
2024-04-04 14:04:51 +0300

    posting prctice

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(userName, userEmail){
+   return  fetch ("http://localhost:3000/users", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        body : JSON.stringify({
+            name : userName,
+            email : userEmail
+        })
+    })
+
+       .then(function (response) {
+        return response.json() 
+        })
+       .then(function (newObj){
+           const body = document.querySelector("body")
+           const div = document.createElement("div")
+           div.innerHTML = newObj.id 
+           body.appendChild(div) 
+        })
+
+        .catch(function(error){
+
+            const body = document.querySelector("body")
+            const div = document.createElement("div")
+            div.innerHTML = error
+            body.appendChild(div)
+        })
+    
+}
